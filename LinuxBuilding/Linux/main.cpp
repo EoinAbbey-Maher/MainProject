d0b2701cdd10598bb0b19c1eae3953d7ca69e031
EoinVm <c00217717@itcarlow.ie>
2019-10-21 02:08:36 +0100

    adding linux

---
+++ LinuxBuilding/Linux/main.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QHBoxLayout>
+
+#include "DragWidget.h"
+
+int main(int argc, char *argv[])
+{
+    Q_INIT_RESOURCE(Linux);
+
+    QApplication a(argc, argv);
+
+    QWidget mainWidget;
+    QHBoxLayout * horizontalLayout = new QHBoxLayout(&mainWidget);
+    horizontalLayout->addWidget(new DragWidget);
+    horizontalLayout->addWidget(new DragWidget);
+
+    mainWidget.setWindowTitle(QObject::tr("Starter Testing"));
+    mainWidget.show();
+
+    return a.exec();
+}
